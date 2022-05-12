415ca9944631ab5970037022c2b767cd2f23f7af
Allison Tong <allisontong04@gmail.com>
2022-05-12 12:00:29 -0700

    The calm before the storm

---
+++ models/Data.js
@@ -0,0 +1,14 @@
+var mongoose = require("mongoose");
+
+var Data = mongoose.model("Info",{
+	name:{
+		required:true,
+		unique:true,
+		type:String,
+	},
+    grade: Number
+});
+
+
+
+module.exports = Data;
