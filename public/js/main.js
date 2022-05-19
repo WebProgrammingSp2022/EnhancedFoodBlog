e25887009a1c87a096219b9b89b048e0979b0920
Sierra Brandt <44011600+SierraBrandt@users.noreply.github.com>
2022-05-19 12:36:56 -0700

    Filters

--- public/js/main.js
+++ public/js/main.js
@@ -9,10 +9,18 @@ function readClicked(){
 
                   //  let ar = [];
                     let pa = document.createElement("pa");
+
                     let p = document.getElementById("parag");
+
+                    let div = document.createElement("div");
+
+
                   //  imgloop();
                     for(let i=0;i<data.val.length;i++)
                     {
+                            let div = document.createElement("div");
+                            let pa = document.createElement("pa");
+                            div.classList.add("file-row")
                             console.log(i)
                             if(data.val[i]==null)
                             {
@@ -28,13 +36,27 @@ function readClicked(){
                             var src = document.getElementById("images");
                             src.appendChild(img);
                             //image.src = "/images/"+ data.val[i].filename2;
-                            p.innerHTML += "<br/>"
-                            p.innerHTML += "<br /> Name:" + data.val[i].name + "<br />"+  "Ingredients:" + data.val[i].ingredients;
-                            p.innerHTML += "<br /> Instructions:" + data.val[i].instructions;
-                            p.innerHTML += "<br /> Allergies:" + data.val[i].allergies;
-                            p.innerHTML += "<br /> Diets:" + data.val[i].diet;
+
+                            pa.innerHTML += "<br/>"
+                            pa.innerHTML += "<br /> Name:" + data.val[i].name + "<br />"+  "Ingredients:" + data.val[i].ingredients;
+                            pa.innerHTML += "<br /> Instructions:" + data.val[i].instructions;
+                            pa.innerHTML += "<br /> Allergies:" + data.val[i].allergies;
+                            for(let j = 0; j < data.val[i].allergies.length; j++){
+                              $(div).addClass(data.val[i].allergies[j]);
+                            }
+                            pa.innerHTML += "<br /> Diets:" + data.val[i].diet;
+
+                            for(let j = 0; j < data.val[i].diet.length; j++){
+                              $(div).addClass(data.val[i].diet[j]);
+                            }
+
+                            div.appendChild(pa);
+
                     }
-                    p.appendChild(pa);
+
+                    div.appendChild(pa);
+                    p.appendChild(div);
+                    //p.classList.add("file-row")
                   }
 
               } ,
@@ -61,4 +83,33 @@ function imgloop()
 }
 $(document).ready(function(){
  readClicked();
+
+
+ function filterFilesList() {
+    	var rows = $('.file-row');
+
+    	var checked = $("#filterControls :checkbox:checked");
+
+    	if(checked.length){
+    		rows.show(200);
+        var type = []
+    		var arr = checked.map(function(){
+    			 type.push("." + $(this).val())
+
+    		}).get();
+        //var selector = arr.join('')
+        //console.log(selector)
+        for (let i = 0; i < type.length; i++){
+          $(type[i]).hide(200);
+        }
+
+
+
+    	} else {
+    		rows.show();
+    	}
+    }
+
+    $("#filterControls :checkbox").click(filterFilesList);
+    filterFilesList();
 });
